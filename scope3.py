7ea73d6a74c0cdc3300158cc3ea1c3f2c9b430ff
Heba Syed <hsyed2017@pingry.org>
2017-04-17 09:07:50 -0400

    scope3.py copied example 3 and commented what we think it will do

---
+++ scope3.py
@@ -0,0 +1,16 @@
+def func2():
+	global s
+	print(s)
+	s = "hello hello"
+	print(s)
+	
+def func1():
+	s = "hi hi"
+	print(s)
+
+s = "Hello World"
+func2()
+print(s)
+
+
+#will print hello world then hello hello then hello world again 
